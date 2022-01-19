0ccaf3358d134368dab60271bdff7993241d9528
Artur <piszczatowski.artur@gmail.com>
2022-01-19 23:45:22 +0100

    Gnuplot revolting

---
+++ cmake-build-debug/CMakeFiles/Lab_12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_12.dir/potential_field_navigator.cpp.obj"
+  "Lab_12.exe"
+  "Lab_12.exe.manifest"
+  "Lab_12.lib"
+  "Lab_12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
